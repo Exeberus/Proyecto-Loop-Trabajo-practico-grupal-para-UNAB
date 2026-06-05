09db0f4e4f4c8733e38ecd50582e82fbfa70f352
Alienbolt Aaaa <megadawnmega@gmail.com>
2026-06-05 01:04:07 -0300

    Primer commit para conexion con front

--- app.py
+++ app.py
@@ -1,33 +1,57 @@
 import sqlite3
-from flask import Flask, render_template, request, session
+from flask_cors import CORS
+from flask import Flask, render_template, request, session, jsonify
 
 app = Flask(__name__)
 app.secret_key = '0f2e4c18ca9ae37290cad43b86fad8f65aad8cf682561b0b3a0650c80737df45'
 
+print("App iniciada")
+CORS(app)
+
 ## Conectar a Base de datos
+@app.route("/api/test")
+def test():
+    return jsonify({
+        "message": "Backend funcionando"
+    })
+
 def get_db():
     conn = sqlite3.connect("database.db")
     return conn
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
+
+    error = None
     
     if request.method == "POST":
 
         username = request.form["username"]
         password = request.form["password"]
 
-        conn = get_db()
-        cursor = conn.cursor()
+        if len(username) < 3:
+            error = "El usuario debe tener al menos 3 caracteres"
+    
+        elif len (password) < 6:
+            error = "La contraseña debe tener al menos 6 caracteres"
 
-        cursor.execute(
-            "INSERT INTO users (username, password) VALUES (?, ?)",
-            (username, password)
-        )
+        elif " " in username:
+            error = "El usuario no puede tener espacios"
 
-        conn.commit()
-        conn.close()
-        return "Usuario registrado"
+        else:
+
+            conn = get_db()
+            cursor = conn.cursor()
+
+            cursor.execute(
+                "INSERT INTO users (username, password) VALUES (?, ?)",
+                (username, password)
+            )
+
+            conn.commit()
+            conn.close()
+
+            return "Usuario registrado correctamente"
 
     return render_template("register.html")
 
@@ -68,10 +92,9 @@ def dashboard():
 @app.route("/logout")
 def logout():
 
-    if "user" in session:
-        session.pop("user", None)
-        return "Sesión cerrada"
+    session.pop("user", None)
 
+    return "Sesión cerrada"
 
 ## Modo de Depuración
 if __name__ == '__main__': 
--- init_db.py
+++ init_db.py
@@ -1,6 +1,11 @@
 import sqlite3
+import os
+
+print("CARPETA ACTUAL:")
+print(os.getcwd())
 
 conn = sqlite3.connect("database.db")
+
 cursor = conn.cursor()
 
 cursor.execute("""
